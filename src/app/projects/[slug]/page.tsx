8c8ce72933e76263144f880dcaa9de257c37496f
sourov-adikari <productscornerofficial@gmail.com>
2026-09-11 15:18:07 +0600

    Update page.tsx

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { getProjectBySlug } from "@/models/Project";
 import type { Project } from "@/types/portfolio";
-import ProjectDetails from "@/components/projects/ProjectDetails";
+import ProjectDetails from "@/components/ProjectDetails/ProjectDetails";
 import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
